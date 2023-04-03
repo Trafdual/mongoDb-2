a3644aff664e2fa579a46fbe88cd82a5c7684eda
trafdual <you@example.com>
2023-04-03 22:56:31 +0700

    finsh-project

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
-var hbs = require('express-handlebars');
+const hbs = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const Handlebars = require('handlebars')
 var bodyParser = require("body-parser");
 var session = require('express-session')
 const mongoose =require('mongoose');
 const uri = "mongodb+srv://traz08102003:vrRJOA6nqwODzFmz@cp17303.4gzmzyt.mongodb.net/Cp17303?retryWrites=true&w=majority";
 app.use(bodyParser.urlencoded({ extended: false }));
- mongoose.connect(uri).then(console.log('kết nối thành công'));
+ mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(console.log('kết nối thành công'));
 app.use(session({
     secret: 'trafdual',
     resave: false,
@@ -16,7 +21,8 @@ app.use(session({
 app.engine('.hbs', hbs.engine({
     extname: "hbs",
     defaultLayout: false,
-    layoutsDir: "views/layouts/"
+    layoutsDir: "views/layouts/",
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
 }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
@@ -40,33 +46,31 @@ app.get('/', async (req, res) => {
   
   // Route to handle "Edit" form submission
   app.post('/edit/:id', async (req, res) => {
-    const {tenSP, giaSP,soluongSP } = req.body
-    const id = req.params.id
-    let obj= await sanphamModal.findById(id);
-    if(req.method="POST"){
-      let obj=new sanphamModal();
-      obj.tenSP=req.body.tenSP;
-      obj.giaSP=req.body.giaSP;
-      obj.soluongSP=req.body.soluongSP;
-      obj._id=id;
-      await sanphamModal.findByIdAndUpdate(id,obj);
-    }
-    res.render('edit',{obj:obj});
-    res.redirect('/');
+    const id = req.params.id;
+    const newData = req.body;
+    // Update data in MongoDB based on ID
+    sanphamModal.findByIdAndUpdate(id, newData)
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('Internal server error');
+      });
+
   })
   app.get('/edit/:id', async(req, res)=>{
-    const {tenSP, giaSP,soluongSP } = req.body
-    const id = req.params.id
-    let obj= await sanphamModal.findById(id);
-    if(req.method="POST"){
-      let obj=new sanphamModal();
-      obj.tenSP=req.body.tenSP;
-      obj.giaSP=req.body.giaSP;
-      obj.soluongSP=req.body.soluongSP;
-      obj._id=id;
-      await sanphamModal.findByIdAndUpdate(id,obj);
-    }
-    res.render('edit',{obj:obj});
+    const id = req.params.id;
+    // Retrieve data from MongoDB based on ID
+    sanphamModal.findById(id)
+      .then(data => {
+        // Render the edit view template with the retrieved data
+        res.render('edit', { data });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('Internal server error');
+      });
 })
   // Route to handle "Delete" request
   app.post('/delete/:id', async (req, res) => {
